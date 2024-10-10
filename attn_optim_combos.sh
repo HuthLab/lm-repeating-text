d7857de6b09cf0cba906eb84d94dbf8d2c0e88cf
Aditya Vaidya <kroq-gar78@users.noreply.github.com>
2024-10-10 14:45:25 -0500

    Fix old arg name in orchestration script

--- attn_optim_combos.sh
+++ attn_optim_combos.sh
@@ -5,7 +5,7 @@
 time for layer in 5 {0..4} {6..11}; do
     time for story in wheretheressmoke fromboyhoodtofatherhood onapproachtopluto eyespy souls; do
         for i in {0..4}; do
-            time python3 ./attn_optim.py --bias_method recent_powerlaw --epochs 2000 --figure_dir attn-optim-results/l${layer}_${story}_${i} --opt_layers ${layer} --train_story $story
+            time python3 ./attn_optim.py --bias_method recent_powerlaw --epochs 2000 --output_dir attn-optim-results/l${layer}_${story}_${i} --opt_layers ${layer} --train_story $story
         done
     done
 done
